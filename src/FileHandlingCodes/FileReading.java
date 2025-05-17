2d314af8e741ff2375997aa69a18d8c6aaafbcae
SAURABHTILAK <saurbhtilak6@gmail.com>
2025-05-17 10:29:16 +0530

    File Rea ding

--- src/FileHandlingCodes/FileReading.java
+++ src/FileHandlingCodes/FileReading.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class FileReading {
 
 	public static void main(String[] args) throws Exception {
-		FileInputStream fl=new FileInputStream("C:\\FileHandle Folder\\read.txt");//"C:\FileHandle Folder\read.txt"
+		FileInputStream fl=new FileInputStream("C:\\FileHandle Folder\\second.txt");//"C:\FileHandle Folder\read.txt"
 		int data;
 		while((data=fl.read())!=-1)
 		{
